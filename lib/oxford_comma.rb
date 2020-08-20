c1bd95373506e477ff8aed3dd287684c826cece9
modern-class-9458 <alexander.clamor@gmail.com>
2020-08-20 20:24:15 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,16 @@
 def oxford_comma(array)
-
+  case array
+  when array.length == 1
+    array[0]
+  when array.length == 2
+    "#{array[0]} and #{array[1]}"
+  else
+    array << "and #{array.last}"
+    array.join(", ")
+  end
 end
+
+array1 = ["A"]
+array2 = ["A","B"]
+array3 = ["A","B","C"]
+puts oxford_comma(array1)
