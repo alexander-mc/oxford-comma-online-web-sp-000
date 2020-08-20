3e620aad933ec51c955c0fe5bf4e46bb0d9e45f4
modern-class-9458 <alexander.clamor@gmail.com>
2020-08-20 20:29:31 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,8 @@
 def oxford_comma(array)
   case array
-  when .length == 1
+  when array.length == 1
     array[0]
-  when .length == 2
+  when array.length == 2
     "#{array[0]} and #{array[1]}"
   else
     array << "and #{array.last}"
